e2ba01eea60502b9cd8595c7ad72d88641bb2902
linzj <zuojian.lzj@alibaba-inc.com>
2023-08-17 17:31:38 +0800

    int./ Incorrect stack maps, missing one stack slot mark ROOTCAUSE: The IR of int./ is similar to
    ```
    blx Double.fromInteger
    %46 = COPY $r0
    %33 = COPY %46
    B0:
    blx num.toDouble (record %33)
    ...
    
    B1:
    %10 = COPY %33
    ...
    B2:
    %7 = COPY %33
    ...
    B3:
    %0 = PHI %10, %7
    ```
    
    Where %46 comes from t74: i32,ch,glue = CopyFromReg t73, Register:i32 $r0 And %33 comes from t82: ch = CopyToReg t0, Register:i32 %33, t74 which is generated by a mechanism like CopyToExportRegsIfNeeded In the pass Optimize machine instruction PHIs, it will find that %0 = PHI %10, %7 actually comes from the same source %46 So all the use points of %0 are replaced by %46 The pass Statepoint Simplify that runs before the pass Optimize machine instruction PHIs will record all the tagged registers. Here it records %33 But after the pass Optimize machine instruction PHIs, %33 is dead and will be removed by later processes.
    A possible ultimate solution is that the pass Statepoint Simplify records not only the simple src, but also the root src, which is to follow up the copy to reach its root. But it cannot guarantee that all subsequent operations have tracking. Losing track will also cause problems, so this solution is abandoned for now. The current workaround is to execute the pass Optimize machine instruction PHIs before executing Statepoint Simplify to avoid this problem.

--- llvm/lib/CodeGen/TargetPassConfig.cpp
+++ llvm/lib/CodeGen/TargetPassConfig.cpp
@@ -900,8 +900,8 @@ void TargetPassConfig::addMachinePasses() {
     addPass(createRegUsageInfoPropPass());
 
   if (getOptimizeRegAlloc()) {
-    addPass(createStatepointSimplifyPass());
     addPass(&OptimizePHIsID, false);
+    addPass(createStatepointSimplifyPass());
   }
   // Run pre-ra passes.
   addPreRegAlloc();
