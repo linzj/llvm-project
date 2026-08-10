f623446766f8f9eab209e0f309f91e57bf278c69
linzj <zuojian.lzj@alibaba-inc.com>
2026-08-10 15:59:42 +0800

    BUGFIX:0:Fix LLVM 10 JSON.h compile error with newer libc++
    ROOTCAUSE: llvm/Support/JSON.h inline-uses std::vector<Value>
    member functions (operator[]/back/front/data/begin/end etc.) while
    Value is still incomplete. Older libc++ tolerated this; the newer
    libc++ in Android NDK r27 hard-errors when instantiating vector
    members on an incomplete type:
      error: subscript of pointer to incomplete type 'llvm::json::Value'
    
    SOLUTION: Declare Array's non-template member functions in-class
    and define them out-of-line (inline) after Value is fully defined,
    matching how upstream handles similar incomplete-type issues.
    Template members (emplace_back/range insert/emplace) stay in-class:
    their instantiation point is at the use site, where Value is
    already complete.

--- llvm/include/llvm/Support/JSON.h
+++ llvm/include/llvm/Support/JSON.h
@@ -168,36 +168,34 @@ public:
       emplace_back(V);
   }
 
-  Value &operator[](size_t I) { return V[I]; }
-  const Value &operator[](size_t I) const { return V[I]; }
-  Value &front() { return V.front(); }
-  const Value &front() const { return V.front(); }
-  Value &back() { return V.back(); }
-  const Value &back() const { return V.back(); }
-  Value *data() { return V.data(); }
-  const Value *data() const { return V.data(); }
+  Value &operator[](size_t I);
+  const Value &operator[](size_t I) const;
+  Value &front();
+  const Value &front() const;
+  Value &back();
+  const Value &back() const;
+  Value *data();
+  const Value *data() const;
 
-  iterator begin() { return V.begin(); }
-  const_iterator begin() const { return V.begin(); }
-  iterator end() { return V.end(); }
-  const_iterator end() const { return V.end(); }
+  iterator begin();
+  const_iterator begin() const;
+  iterator end();
+  const_iterator end() const;
 
-  bool empty() const { return V.empty(); }
-  size_t size() const { return V.size(); }
-  void reserve(size_t S) { V.reserve(S); }
+  bool empty() const;
+  size_t size() const;
+  void reserve(size_t S);
 
-  void clear() { V.clear(); }
-  void push_back(const Value &E) { V.push_back(E); }
-  void push_back(Value &&E) { V.push_back(std::move(E)); }
+  void clear();
+  void push_back(const Value &E);
+  void push_back(Value &&E);
   template <typename... Args> void emplace_back(Args &&... A) {
     V.emplace_back(std::forward<Args>(A)...);
   }
-  void pop_back() { V.pop_back(); }
+  void pop_back();
   // FIXME: insert() takes const_iterator since C++11, old libstdc++ disagrees.
-  iterator insert(iterator P, const Value &E) { return V.insert(P, E); }
-  iterator insert(iterator P, Value &&E) {
-    return V.insert(P, std::move(E));
-  }
+  iterator insert(iterator P, const Value &E);
+  iterator insert(iterator P, Value &&E);
   template <typename It> iterator insert(iterator P, It A, It Z) {
     return V.insert(P, A, Z);
   }
@@ -205,7 +203,7 @@ public:
     return V.emplace(P, std::forward<Args>(A)...);
   }
 
-  friend bool operator==(const Array &L, const Array &R) { return L.V == R.V; }
+  friend bool operator==(const Array &L, const Array &R);
 };
 inline bool operator!=(const Array &L, const Array &R) { return !(L == R); }
 
@@ -484,6 +482,40 @@ private:
 
 bool operator==(const Value &, const Value &);
 inline bool operator!=(const Value &L, const Value &R) { return !(L == R); }
+
+// Out-of-line definitions of Array's non-template members. They are only
+// declared inside class Array because std::vector<Value> member functions
+// require Value to be complete (enforced by newer libc++ versions).
+inline Value &Array::operator[](size_t I) { return V[I]; }
+inline const Value &Array::operator[](size_t I) const { return V[I]; }
+inline Value &Array::front() { return V.front(); }
+inline const Value &Array::front() const { return V.front(); }
+inline Value &Array::back() { return V.back(); }
+inline const Value &Array::back() const { return V.back(); }
+inline Value *Array::data() { return V.data(); }
+inline const Value *Array::data() const { return V.data(); }
+
+inline Array::iterator Array::begin() { return V.begin(); }
+inline Array::const_iterator Array::begin() const { return V.begin(); }
+inline Array::iterator Array::end() { return V.end(); }
+inline Array::const_iterator Array::end() const { return V.end(); }
+
+inline bool Array::empty() const { return V.empty(); }
+inline size_t Array::size() const { return V.size(); }
+inline void Array::reserve(size_t S) { V.reserve(S); }
+
+inline void Array::clear() { V.clear(); }
+inline void Array::push_back(const Value &E) { V.push_back(E); }
+inline void Array::push_back(Value &&E) { V.push_back(std::move(E)); }
+inline void Array::pop_back() { V.pop_back(); }
+inline Array::iterator Array::insert(iterator P, const Value &E) {
+  return V.insert(P, E);
+}
+inline Array::iterator Array::insert(iterator P, Value &&E) {
+  return V.insert(P, std::move(E));
+}
+
+inline bool operator==(const Array &L, const Array &R) { return L.V == R.V; }
 
 /// ObjectKey is a used to capture keys in Object. Like Value but:
 ///   - only strings are allowed
